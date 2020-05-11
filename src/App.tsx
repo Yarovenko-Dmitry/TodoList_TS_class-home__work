22a523b50fdf53e6e8b73739a4cdcf9ef677f50b
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-11 14:08:34 +0300

    RESTART_TodoList_TS_1_classwork_11-05-2020

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import styles from './App.module.css';
-import TodoList from './components/Todolist/Todolist'
+import './App.css';
+import {TodoList} from "./TodoList";
+
 
 function App() {
-    return (
-        <div className={styles.App}>
-            <TodoList/>
-            <TodoList/>
-            <TodoList/>
-        </div>
-    );
+
+  const tasks1 = [
+    {id: 1, title: 'HTML&CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: true},
+    {id: 3, title: 'ReactJS', isDone: false},
+  ];
+
+  const tasks2 = [
+    {id: 1, title: 'Hello world', isDone: true},
+    {id: 2, title: 'I am happy', isDone: false},
+    {id: 3, title: 'Yo', isDone: false},
+  ];
+
+  return (
+    <div className="App">
+      <TodoList title={'What to lern'} tasks={tasks1}/>
+      <TodoList title={'Songs'} tasks={tasks2}/>
+    </div>
+  );
 }
 
 export default App;
