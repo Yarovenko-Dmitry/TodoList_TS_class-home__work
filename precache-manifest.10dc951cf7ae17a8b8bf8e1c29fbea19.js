8d6afbf65c71186a18b781b4acbaa1306824a284
prostiak2019 <yarovenkodmv@gmail.com>
2020-07-04 15:32:22 +0300

    Updates

---
+++ precache-manifest.10dc951cf7ae17a8b8bf8e1c29fbea19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74ada90d8c16d78dd718143d0063460",
+    "url": "/TodoList_TS_class-home__work/index.html"
+  },
+  {
+    "revision": "6031e820f1286e9a9004",
+    "url": "/TodoList_TS_class-home__work/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "47263b99a2da70794dbb",
+    "url": "/TodoList_TS_class-home__work/static/js/2.04e88616.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList_TS_class-home__work/static/js/2.04e88616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6031e820f1286e9a9004",
+    "url": "/TodoList_TS_class-home__work/static/js/main.4073c0a3.chunk.js"
+  },
+  {
+    "revision": "f8bcaa339bc5f13c12dd",
+    "url": "/TodoList_TS_class-home__work/static/js/runtime-main.08535e7a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd16c8112250b7959352b9505f3e7610.js"
+  "/TodoList_TS_class-home__work/precache-manifest.10dc951cf7ae17a8b8bf8e1c29fbea19.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TodoList_TS_class-home__work/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
