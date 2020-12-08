adbd39614df9961e0b6c0f81d896023a26fa53e2
prostiak2019 <yarovenkodmv@gmail.com>
2020-12-08 15:22:51 +0300

    TodoList_TS_class-home__work_17.0__08-12-2020: Redux Toolkit, createSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,20 +1,31 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import {combineReducers} from 'redux'
 import {appReducer} from './app-reducer'
-import {authReducer} from '../features/Login/auth-reducer';
+import {authReducer} from '../features/Login/auth-reducer'
+import {configureStore} from '@reduxjs/toolkit'
+import thunkMiddleware from 'redux-thunk';
+
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
-    app: appReducer,
-    auth: authReducer
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer
 })
+
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// export const store = createStore(rootReducer, applyMiddleware());
+
+// непосредственно создаём store но уже через toolkit
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+});
+
+
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
--- src/components/AddItemForm/AddItemForm.stories.tsx
+++ src/components/AddItemForm/AddItemForm.stories.tsx
@@ -3,18 +3,19 @@ import {action} from '@storybook/addon-actions'
 import {AddItemForm} from './AddItemForm'
 
 export default {
-  title: 'AddItemForm Stories',
-  component: AddItemForm
+    title: 'AddItemForm Stories',
+    component: AddItemForm
 }
 
 export const AddItemFormBaseExample = (props: any) => {
-  return (<AddItemForm
-    addItem={action('Button inside form clicked')}
-  />)
+    return (<AddItemForm
+        addItem={action('Button inside form clicked')}
+    />)
 }
 
+
 export const AddItemFormDisabledExample = (props: any) => {
-  return (<AddItemForm disabled={true}
-                       addItem={action('Button inside form clicked')}
-  />)
+    return (<AddItemForm disabled={true}
+        addItem={action('Button inside form clicked')}
+    />)
 }
